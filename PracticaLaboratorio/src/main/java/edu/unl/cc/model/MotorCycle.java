b10e3dbd699e5d170ef0439cdaad77214f71daa0
TheGreatKamus <emiliofloresga@gmail.com>
2025-05-28 20:25:18 -0500

    herencia motorcycle

--- PracticaLaboratorio/src/main/java/edu/unl/cc/model/MotorCycle.java
+++ PracticaLaboratorio/src/main/java/edu/unl/cc/model/MotorCycle.java
@@ -1,7 +1,8 @@
 package edu.unl.cc.model;
 
-public class MotorCycle {
+public class MotorCycle extends Vehicle{
     private int displacement; //cilindrada 
     private static double costMaintenance; 
+    
     
 }
