9719c72c555915afff24108f0b375fd61dd094bf
TheGreatKamus <emiliofloresga@gmail.com>
2025-05-30 20:09:40 -0500

    correciones de clases

--- PracticaLaboratorio/src/main/java/edu/unl/cc/model/MotorCycle.java
+++ PracticaLaboratorio/src/main/java/edu/unl/cc/model/MotorCycle.java
@@ -5,30 +5,41 @@ import java.time.LocalDate;
 public class MotorCycle extends Vehicle {
 
     private int displacement; //cilindrada 
-    private static final double costMaintenance = 30.0;
+    private static final double COST_MAINTENANCE = 400.0;
+    private static final float CONSUMPTION_FUEL = 0.3f;
+    
+    public MotorCycle(int displacement, String credential, double tonnage, float consumptionFuel, LocalDate lastFixDateMaintenance) {
+        super(credential, tonnage, CONSUMPTION_FUEL, lastFixDateMaintenance);
+        this.displacement = displacement;
+    }
 
-    public MotorCycle(String credential, double tonnage, float consumptionFuel, LocalDate lastFixDateMaintenance) {
-        super(credential, tonnage, consumptionFuel, lastFixDateMaintenance);
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
     }
 
     @Override
     void loadFuel() {
-        System.out.println("Cargando " + getConsumptionFuel() + " L/km");
+        System.out.println("Cargando combustible: " + getConsumptionFuel() + " L/km");
     }
 
-    double getCostMaintenance() {
-        return costMaintenance;
+    @Override
+    public double getCostMaintenance() {
+        return COST_MAINTENANCE;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("-Motocicleta- ");
+        sb.append("-Motocicleta- \n");
         sb.append("Cilindrada: ").append(displacement);
-        sb.append("Placa: ").append(getCredential());
-        sb.append("Tonelaje").append(getTonnage());
-        sb.append("Combustible: ").append(getConsumptionFuel());
-        sb.append("ultima fecha de mantenimietno: ").append(getLastFixDateMaintenance());
+        sb.append("\nPlaca: ").append(getCredential());
+        sb.append("\nTonelaje").append(getTonnage());
+        sb.append("\nCombustible: ").append(getConsumptionFuel());
+        sb.append("\nUltima fecha de mantenimietno: ").append(getLastFixDateMaintenance());
         return sb.toString();
     }
 }
