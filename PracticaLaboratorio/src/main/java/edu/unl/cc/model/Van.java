35cc7fdc3c4b5090ba4cf37175a6b39ab699d8ec
AndersonUNL <anderson.coello@unl.edu.ec>
2025-05-30 19:27:14 -0500

    Voy a subir las clases Van y RegisterVehicle

--- PracticaLaboratorio/src/main/java/edu/unl/cc/model/Van.java
+++ PracticaLaboratorio/src/main/java/edu/unl/cc/model/Van.java
@@ -1,5 +1,23 @@
 package edu.unl.cc.model;
 
-public class Van {
+import java.time.LocalDate;
 
+public class Van extends Vehicle {
+
+    private boolean dobleTraccion; //simple (4*2) o doble (4*4)
+    private static double costMaintenance;
+
+    @Override
+    void loadFuel() {
+        System.out.println("Cargando " + getConsumptionFuel());
+    }
+
+    @Override
+    double getCostMaintenance() {
+        return 0;
+    }
+
+    public Van(String credential, double tonnage, float fuel, LocalDate lastFixDateMaintenance) {
+        super(credential, tonnage, fuel, lastFixDateMaintenance);
+    }
 }
